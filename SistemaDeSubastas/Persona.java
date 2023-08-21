6d2b70d7c9e4a33d4ebc809459ca342ee61dd1e3
RuddyMisme <142124669+RuddyMisme@users.noreply.github.com>
2023-08-20 22:58:16 -0400

    primer commit

---
+++ SistemaDeSubastas/Persona.java
@@ -0,0 +1,15 @@
+public class Persona {
+    
+    private String nombre;
+    public Persona(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void setNomnbre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+}
